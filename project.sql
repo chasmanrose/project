e60213cb5c804eaa3aedf0a0606dd32a963b09a4
chasmanrose <chasmanrose@gmail.com>
2016-12-02 21:48:33 -0500

    Update project.sql

--- project.sql
+++ project.sql
@@ -12,85 +12,74 @@ SET ECHO ON
 --Orders Table details order information 
 ----------------------------------------------------------------------------------------------------
 CREATE TABLE Orders(
-	OrderID CHAR(6) NOT NULL, -- Made this change to avoid aggregation issues; PK
-	PayMethod String, —-May need to change to CHAR, but length changes
-	ShippingAddress String, —-comment above
-	cID CHAR(12) NOT NULL, -- foreign key to Customers Table
+	OrderID String, -- Made this change to avoid aggregation issues; PK
+	PayMethod String,
+	ShippingAddress String,
+	cID String, -- foreign key to Customers Table
 
-	ActualShipDate DATE NOT NULL,
-	ExpectedShipDate DATE NOT NULL,
-	ReceivedDate DATE NOT NULL,
+	ActualShipDate datetime,
+	ExpectedShipDate datetime,
+	ReceivedDate datetime,
 
-	Pickup BOOLEAN NOT NULL,
+	Pickup Boolean,
 	
-	Discount Float, —-not sure if ‘float’ is correct, or if integer is
-	CONSTRAINT sIC1 PRIMARY KEY (OrderID) —-PRIMARY KEY(OrderID)
+	Discount Float,
+	PRIMARY KEY(OrderID)
+	
+	-- Every Order must have a customer
+	CONSTRAINT ORD_IC1 FOREIGN KEY, (cID),
+	CONSTRAINT ORD_IC2 CHECK (ExpectedShipDate => ReceivedDate),
+	CONSTRAINT ORD_IC3 CHECK (ActualShipDate +. ReceivedDate),
+	CONSTRAINT ORD_IC4 CHECK (PayMethod IN 'credit', 'check', 'NEFT', 'IMPS', 'RTGS');
+	CONSTRAINT ORD_IC5 CHECK (Discount <= 1 AND Discount >= 0)
 );
 ---------------------------------------------------------------------------------------------------------------
 CREATE TABLE Customers(
-	Class CHAR(1) NOT NULL,
-	cName String, —-not sure of CHAR(size) here or just can have string
-	cID CHAR(12) NOT NULL,
-	cLocation String, —-same as above
-	CONSTRAINT sIC2 PRIMARY KEY (cID) —-PRIMARY KEY(cID)
-);
-------------------------------- ------------------------------------------------------------------------------------
-CREATE TABLE Employee(
-	Job CHAR(20) NOT NULL,
-	EmployeeID CHAR(11) NOT NULL, -- Again changes made for aggregation purposes don't want to get sum of emp ID, just cnt
-	NameFirst String, —-not sure again of length of CHAR or can just have string
-	NameLast String, —-same as above
-	LocationID String, -- FK to Location Table
-	
-	PayRate Float, -- Change made because I wasn't sure if this is an efficiency rate. 
-			—-not sure if FLOAT is a type, or if we have to use INTEGER
-		       -- if so, calculation would be better generated with a Query
-	CONSTRAINT sIC3 PRIMARY KEY (EmployeeID) -—PRIMARY KEY(EmployeeID)
+	Class String,
+	cName String,
+	cID String,
+	cLocation String, 
+	PRIMARY KEY(cID)
+
+	Cust_IC1 CHECK (class IN 'individual', 'company', 'wholesale'),
 );
 ----------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Location(
-	LocationID CHAR(4) NOT NULL,
-	LocationName String, —-will fix when other like questions are answered
+	LocationID String,
+	LocationName String,
 	Address String,
 	City String,
 
-	ZipCode INTEGER NOT NULL,
-	Latitude INTEGER NOT NULL, -- Wanted to add these just for functionality, map building!
-	Longitude INTEGER NOT NULL,
-	CONSTRAINT sIC4 PRIMARY KEY (LocationID) —-PRIMARY KEY(LocationID)
+	ZipCode Integer,
+	Latitude Integer, -- Wanted to add these just for functionality, map building!
+	Longitude Integer,
+	PRIMARY KEY(LocationID)
 );
 ------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Line(
-	LineNumber CHAR(3) NOT NULL,
-	OrderID CHAR(6) NOT NULL, —-FK->Order	
-	EmployeeID CHAR(11) NOT NULL, -- FK -> Employee 
+	LineNumber String,
+	OrderID String,	
+	OrderID String, -- FK -> Order
+	EmployeeID String, -- FK -> Employee 
 
-	LinePrice Float, -- MV? —-set as float, can have decimals
-	QuantityRequested INTEGER NOT NULL,
-	QuantitySelected INTEGER NOT NULL,
-	CONSTRAINT sIC5 PRIMARY KEY (OrderID, LineNumber)—-PRIMARY KEY(OrderID, LineNumber)
+	LinePrice Integer, -- MV?
+	QuantityRequested Integer,
+	QuantitySelected Integer,
+	PRIMARY KEY(OrderID, LineNumber)
 	
+	LN_IC1 CHECK (QuantityRequested > 0),
+	LN_IC2 CHECK (QuantitySelected >= 0),
+	LN_IC3 CHECK (LinePrice >= 0),
 );
 
 --------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Part(
-	PartID CHAR(10) NOT NULL,
-	VendorID CHAR(3) NOT NULL,
-
-	PartPrice Float, 
-	CONSTRAINT sIC7 PRIMARY KEY (PartID) —-PRIMARY KEY(PartID)
-);
---------------------------------------------------------------------------------------------------------------------
-CREATE TABLE CompatibleCar(
-	PartID CHAR(10) NOT NULL,
-	Make String,
-	Model String, 
-	FromYear INTEGER NOT NULL,
-	ToYear INTEGER NOT NULL,
-	Part Price Float,
+	PartID String,
+	VendorID String,
 	
-	QuantityInStock INTEGER NOT NULL,
-	CONSTRAINT sIC9 PRIMARY KEY (BinID) -—PRIMARY KEY(BinID)
+	QuantityAvailable Integer,
+	PartPrice Float,
+	PRIMARY KEY(PartID)
 );
 --------------------------------------------------------------------------------------------------------------------
 CREATE TABLE CompatibleCar(
@@ -105,23 +94,22 @@ CREATE TABLE CompatibleCar(
 );
 ------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Bin(
-	BinID CHAR(6) NOT NULL,
-	LocationID CHAR(4) NOT NULL,
+	BinID String,
+	LocationID String,
 	
-	QuantityInStock INTEGER NOT NULL,
-	CONSTRAINT sIC9 PRIMARY KEY (BinID) -—PRIMARY KEY(BinID)
+	QuantityInStock Integer,
+	PRIMARY KEY(BinID)
 );
 -------------------------------------------------------------------------------------------------------------
 CREATE TABLE Vendors(
-	vID CHAR(3) NOT NULL,
+	vID String,
 	vName String,
 	vAddress String,
 	vCity String,
-	vState CHAR(2) NOT NULL,
-	vZIP INTEGER NOT NULL
-	
-	CONSTRAINT sIC10 PRIMARY KEY (vID) —-PRIMARY KEY(vID)
-
+	vState String,
+	vZip Integer,
+	--vPhone Integer,
+	PRIMARY KEY(vID)
 );
 --------------------------------------------------------------------------------------------------------------
 SET FEEDBACK OFF
